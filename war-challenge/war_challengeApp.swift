53dabf622fe685405eabfbf3da2f661bdeefaf0b
Wahidullah Rahimi <wahidullahrahimi@Wahidullahs-MacBook-Pro.local>
2023-02-19 13:34:54 -0800

    first Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Wahidullah Rahimi on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
